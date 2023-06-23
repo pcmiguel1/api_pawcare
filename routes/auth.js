bd26e45aab43dcf5821421bb728909d894465776
Miguel Amaro <miguel.amaro@worldit.pt>
2023-06-23 19:25:46 +0100

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -83,10 +83,8 @@ router.post("/login", authenticateToken, async (req, res) => {
 
 router.post("/register", authenticateToken, upload.single('image'), async (req, res) => {
 
-    console.log(req.body);
-
     //LETS VALIDATE THE DATA BEFORE WE A USER
-    const { fullname, dateOfBirth, phoneNumber, email, password } = req.body;
+    const { fullname, dateOfBirth, phoneNumber, email, password } = req.body.user;
     if (!fullname) return res.status(422).json({ message: 'fullname is required!' })
     if (!dateOfBirth) return res.status(422).json({ message: 'dateOfBirth is required!' })
     //if (!phoneNumber) return res.status(422).json({ message: 'phoneNumber is required!' })
