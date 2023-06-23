a3f7857de19919c64ba36d7b79b70e652a768367
Miguel Amaro <miguel.amaro@worldit.pt>
2023-06-23 21:02:19 +0100

    log

--- routes/auth.js
+++ routes/auth.js
@@ -84,12 +84,10 @@ router.post("/login", authenticateToken, async (req, res) => {
 router.post("/register", authenticateToken, upload.single('image'), async (req, res) => {
 
     console.log("akdfkfkf");
-    console.log(req.body.user);
+    console.log(req.body);
 
     //LETS VALIDATE THE DATA BEFORE WE A USER
-    const { fullname, dateOfBirth, phoneNumber, email, password } = req.body.user;
-
-    console.log(req.body.user["fullname"]);
+    const { fullname, dateOfBirth, phoneNumber, email, password } = req.body;
 
     if (!fullname) return res.status(422).json({ message: 'fullname is required!' })
     if (!dateOfBirth) return res.status(422).json({ message: 'dateOfBirth is required!' })
